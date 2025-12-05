2ac021b8555309b4b073c48ef1bd6b7d20cf547d
Morgan Burke <morganlemburke@gmail.com>
2025-12-05 04:33:27 +0000

    Finalised welcome screen, added a "helper" and animated page

--- src/pages/warningWelcome.jsx
+++ src/pages/warningWelcome.jsx
@@ -1,6 +1,6 @@
 ﻿import "../styles/warningWelcome.css";
 import warningImg from "../assets/imgs/wii_warning.png";
-
+import normCursor from "../assets/imgs/cursor-normal2.png";
 const warningWelcome = () => {
 
     //const [showLanding, setShowLanding] = useState(true)
@@ -14,7 +14,6 @@ const warningWelcome = () => {
                 role="dialog"
                 aria-labelledby="landing-title">
 
-
                 <div className="landing-inner">
                     <h1 id="landing-title" className="warning-title">
                         <img src={warningImg} alt="Wii warning icon" className="warning-icon" />
@@ -22,9 +21,9 @@ const warningWelcome = () => {
                     </h1>
 
                     <div className="warning-body">
-                        <p>WHILST EXPLORING, PLEASE REVIEW THE CONTENTS OF THIS SITE <br/> FOR INFORMATION ABOUT
+                        <p>PLEASE REVIEW THE CONTENTS OF THIS SITE <br/> FOR INFORMATION ABOUT
                         <span className="morgan"> MORGAN BURKE </span> 
-                         <br/>TO LEARN MORE ABOUT HIM.</p>
+                         <br/>HIS PAST PROJECTS AND BACKGROUND.</p>
                     </div>
 
                     <p className="warning-small">
@@ -32,9 +31,9 @@ const warningWelcome = () => {
                         <a href="https://www.linkedin.com/in/morgan--burke/" target="_blank" rel="noreferrer">www.linkedin.com/morgan--burke</a>
 
                     </p>
-
-
-
+                    <p className="landing-hint">Click
+                        <img src={normCursor} className="clickCursorImg" />
+                        to continue.</p>
                 </div>
             </div>
 
